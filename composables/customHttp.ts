406cd266da8075e465b109192604df76b79ce1fc
Sarthak Jain <sarthak.jain@comprotechnologies.com>
2023-12-17 14:41:15 +0530

    added /

--- composables/customHttp.ts
+++ composables/customHttp.ts
@@ -1,7 +1,7 @@
 export default function customHttp() {
   const customFetch = async(url: string, method: any, options?: { headers?: any, body?: any, query?: any }, token?: any) => {
     // const baseUrl: string = 'http://localhost:3000/';
-    const baseUrl: string = 'https://frozenkart-backend-c21e442c2596.herokuapp.com';
+    const baseUrl: string = 'https://frozenkart-backend-c21e442c2596.herokuapp.com/';
     url = baseUrl + url;
   
     const headers = options?.headers || {};
